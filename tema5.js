82cc27684fa42fc596daf68a4043f7f0c7d9d21e
Jose Manuel Graterol Rodriguez <josemanuelgraterolrodriguez@gmail.com>
2023-02-12 13:41:01 -0400

    Animaciones mobile navbar

--- tema5.js
+++
@@ -1,6 +0,0 @@
-let paragraph = document.getElementById("parrafo")
-let titulo = document.getElementById("cambio")
-let mensaje = prompt("Escribe algo miamor")
-
-paragraph.innerHTML = "cambie pa"
-titulo.innerHTML = mensaje
