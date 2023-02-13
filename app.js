763f9e03925e6393e98922b2f37fe40ea6f1988a
Jose Manuel Graterol Rodriguez <josemanuelgraterolrodriguez@gmail.com>
2023-02-13 12:55:45 -0400

    Cambios responsive y añadido un carrusel de proyectos

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Sticky navbar
+
 window.onscroll = function() {
 	navScroll()
 }
@@ -13,6 +15,8 @@ function navScroll() {
 		document.getElementById("navbar").style.position = "fixed"
 	}
 }
+
+//Menu Mobile
 
 let iconMenu = document.getElementById("menu")
 let closeMenu = document.getElementById("close-menu")
@@ -33,6 +37,55 @@ function closeMobileMenu() {
 	setTimeout(function(){
 		mobileNav.style.display = "none"
 	}, 500)
-		
-	
 }
+
+//Carrousel
+"use strict";
+// Select all slides
+const slides = document.querySelectorAll(".slide");
+
+// loop through slides and set each slides translateX
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+// select next slide button
+const nextSlide = document.querySelector(".btn-next");
+
+// current slide counter
+let curSlide = 0;
+// maximum number of slides
+let maxSlide = slides.length - 1;
+
+// add event listener and navigation functionality
+nextSlide.addEventListener("click", function () {
+  // check if current slide is the last and reset current slide
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  //   move slide by -100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+// select next slide button
+const prevSlide = document.querySelector(".btn-prev");
+
+// add event listener and navigation functionality
+prevSlide.addEventListener("click", function () {
+  // check if current slide is the first and reset current slide to last
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+
+  //   move slide by 100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
